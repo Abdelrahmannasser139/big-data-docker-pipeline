425e86b6589ecf679c0d09e46e82b7981c2bee98
Abdelrahmannasser139 <112584805+Abdelrahmannasser139@users.noreply.github.com>
2024-11-13 20:01:43 +0200

    Update final.sh

--- final.sh
+++ final.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Define container and local directories
 CONTAINER_DIR="/home/doc-bd-a1"
 LOCAL_DIR="."
 
-# Copy output files
 docker cp bd-a1-container:${CONTAINER_DIR}/res_dpre.csv ${LOCAL_DIR}
 docker cp bd-a1-container:${CONTAINER_DIR}/eda-in-1.txt ${LOCAL_DIR}
 docker cp bd-a1-container:${CONTAINER_DIR}/eda-in-2.txt ${LOCAL_DIR}
